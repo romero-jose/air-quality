898faca8d6584de1688a77bf6c42e592b8b1ddb5
José Luis Romero <jose.romero@ing.uchile.cl>
2026-06-28 22:59:34 -0400

    adds drizzle orm

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+
+const main = async () => {
+    if (!process.env.DATABASE_URL) {
+        throw new Error('DATABASE_URL environment variable is not set')
+    }
+    const client = postgres(process.env.DATABASE_URL, { prepare: false })
+    const db = drizzle({ client })
+}
+
+main()
