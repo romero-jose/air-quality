6d1103ce62cdbd98f9aaebc0f1171c9744063693
José Luis Romero <jose.romero@ing.uchile.cl>
2026-07-03 20:11:30 -0400

    remove card from homepage

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,18 +1,10 @@
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
-import { StationsMap } from "./StationsMap"
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { StationsMap } from "./StationsMap";
 
 export const Home = () => {
   return (
     <div className="flex flex-col gap-4">
-      <h1 className="font-heading text-xl font-semibold">Overview</h1>
-      <Card>
-        <CardHeader>
-          <CardTitle>Station map</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <StationsMap />
-        </CardContent>
-      </Card>
+      <StationsMap />
     </div>
-  )
-}
+  );
+};
